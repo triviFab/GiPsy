bae157f8b728432e3c0d020a989eecee0d7e3358
triviFab <fabio.trivisonno@gmail.com>
2017-06-27 21:31:37 +0200

    other files

---
+++ GiPsy-be/src/main/java/com/trivifab/gipsybe/GiPsyBeApplication.java
@@ -0,0 +1,12 @@
+package com.trivifab.gipsybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiPsyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiPsyBeApplication.class, args);
+	}
+}
---
+++ GiPsy-be/src/main/java/com/trivifab/gipsybe/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.trivifab.gipsybe;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GiPsyBeApplication.class);
+	}
+
+}
